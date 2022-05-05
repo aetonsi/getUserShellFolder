28043c9324f297a714a77c8da5a71ccf9f567c5e
aetonsi <18366087+aetonsi@users.noreply.github.com>
2022-05-05 15:29:52 +0200

    first commit

---
+++ getUserShellFolder/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        If My.Application.CommandLineArgs.Count = 0 Then
+            Exit Sub
+        End If
+        Console.WriteLine(My.Computer.Registry.GetValue("HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders", My.Application.CommandLineArgs(0), ""))
+    End Sub
+
+End Module
